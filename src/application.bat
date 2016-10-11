4dd4231762c8b0b9bfb983bd3c5e7245e1f855c7
LucasB <lucas975@hotmail.fr>
2016-10-11 16:04:32 +0200

    Add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,6 @@
- ### application.bat - 201x - BOSSARD ###
- echo "Copyright BOSSARD - 201x"
- echo "Build : 10:00"
- echo "version 1.1"
+### application.bat - 201x - Bossard ###
+version = "2"
+build_hour = "11H00"
+
+echo "Build ${build_hour}"
+echo "version ${version}"
