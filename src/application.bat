9660fa98300d96f1a16cfcc79cb02cc5ecfee8f9
LucasB <lucas975@hotmail.fr>
2016-10-11 16:23:04 +0200

    fix bug

--- src/application.bat
+++ src/application.bat
@@ -1,6 +1,5 @@
 ### application.bat - 201x - Bossard ###
 version = "2"
 build_hour = "11H00"
-
 echo "Build ${build_hour}"
 echo "version ${version}"
---
+++ test/test_app.bat
@@ -0,0 +1,4 @@
+ ### application.bat - 201x - BOSSARD ###
+ echo "Copyright BOSSARD - 201x"
+ echo "Build : 10:00"
+ echo "version 1.2"
